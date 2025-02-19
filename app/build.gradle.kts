4f6be7d103e534db2a4f41a290cdee07bb762937
Jake N <jnewgard@mcards.com>
2025-02-19 14:22:18 -0800

    Clean up gradle files & bump bom

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,9 +44,8 @@ android {
 }
 
 dependencies {
-    //implementation(libs.androidx.core.ktx)
     implementation(libs.material)
-    //implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
 
     implementation(platform(libs.sdk.bom))
