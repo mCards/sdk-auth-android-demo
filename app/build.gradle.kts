b4f481cf2c99b0a7d707daf9e41133db23e540de
jnewgard <44853169+jnewgard@users.noreply.github.com>
2025-02-20 10:43:00 -0800

    Mcards-6162 Update bom (#4)
    * Prune dependencies and update readme
    
    * Clean up gradle files & bump bom
    
    ---------
    
    Co-authored-by: Jake N <jnewgard@mcards.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,15 +44,10 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
     implementation(libs.material)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
 
     implementation(platform(libs.sdk.bom))
     implementation(libs.auth)
-
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
